296b7d5fd3062467dfaa5be524d1a27e86ca3411
Brainiac11 <saathvikkamma@gmail.com>
2026-06-18 16:01:09 -0500

    Got the docker container working and ready. This is going to be great.

---
+++ entrypoint.sh
@@ -0,0 +1,4 @@
+sudo apt update
+sudo apt upgrade
+ros2 wtf
+sleep infinity
---
+++ src/point_cloud/src/point_cloud_fusion_node.cpp
@@ -0,0 +1,8 @@
+#include <memory>
+#include <string>
+#include <vector>
+#include "rclcpp/rclcpp.hpp"
+
+int main(){
+    return 0;
+};
